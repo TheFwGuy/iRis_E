74d3154ae97f0af0dd341520dae0de1bb19c73d0
TheFwGuy <thefwguy@apkh.org>
2015-12-21 23:23:03 -0600

    Added second sensor reading - calibration - menu selection

--- iRis/iRis.ino
+++ iRis/iRis.ino
@@ -37,7 +37,8 @@ LiquidCrystal_I2C  lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin
 Servo rightEye;		/* create servo object to control a servo for the right eye */
 Servo leftEye;		/* create servo object to control a servo for the left eye */
 
-Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+Adafruit_TSL2561_Unified tsl_R = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+Adafruit_TSL2561_Unified tsl_L = Adafruit_TSL2561_Unified(TSL2561_ADDR_LOW, 12345);
 
 /*
  *  I/O defines
@@ -70,12 +71,16 @@ Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 1234
  *  SCL P1_6 Pin 13 
  */
 
+#define NORMAL  0
+#define CALIBRATING 1
+#define SETTING 2
 
 /*
  *  Global variables
  */
 
 char buffer[20];		/* Generic buffer for display purpose */
+char main_status = NORMAL;
 short Encoder_value = 0;
 short Old_encoder_value = 0;
 short Pwm_R_position = MIN_PWM_VALUE;
@@ -109,10 +114,12 @@ void configureSensor(void)
   /* You can also manually set the gain or enable auto-gain support */
   // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
   // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
-  tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
+  tsl_R.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
+  tsl_L.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
   
   /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
-  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  tsl_R.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  tsl_L.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
 
@@ -122,7 +129,7 @@ void configureSensor(void)
 void setup()
 {
    rightEye.attach(EYE_RIGHT_PWM);	/* attaches the servo to the pin */
-   leftEye.attach(EYE_LEFT_PWM);		/* attaches the servo to the pin */
+   leftEye.attach(EYE_LEFT_PWM);        /* attaches the servo to the pin */
 
    // put your setup code here, to run once:
    pinMode(LED_INFO,OUTPUT);
@@ -148,7 +155,7 @@ void setup()
    lcd.clear();
    lcd.home ();                   // go home
    lcd.display();
-   lcd.print("     iRis       ");  
+   lcd.print("iRis Run        ");  
    lcd.setCursor ( 0, 1 );        // go to the next line
    lcd.print ("TheFwGuy    1.0");      
 
@@ -163,41 +170,100 @@ void setup()
 
 void loop()
 {
-   /* Get a new sensor event */ 
-   sensors_event_t event;
-   tsl.getEvent(&event);
- 
-   /* Display the results (light is measured in lux) */
-   if (event.light)
+   if(KEYIsPressed(SELBTN))
    {
-      lcd.setCursor ( 0, 1 );        // go to the next line
-      lcd.print ("Light :           ");      
-      lcd.setCursor ( 8, 1 );
-      lcd.print(event.light);
+      while(KEYIsPressed(SELBTN));
+      switch(main_status)
+      {
+         case NORMAL:
+            lcd.home ();                   // go home
+            lcd.print("iRis Calibrating");  
+            main_status = CALIBRATING;
+            break;
 
-      Pwm_R_position = map(event.light, 0, MAX_LIGHT_VALUE, MIN_PWM_VALUE, MAX_PWM_VALUE);
-      Pwm_L_position = map(event.light, 0, MAX_LIGHT_VALUE, MIN_PWM_VALUE, MAX_PWM_VALUE);
-
-      rightEye.write(Pwm_R_position);
-      leftEye.write(Pwm_L_position);
-      delay(10);
-
-   }
-  
-   if(Encoder_value != Old_encoder_value)
-   {
-      Old_encoder_value = Encoder_value;
-      Pwm_R_position = map(Encoder_value, 0, MAX_ENCODER_VALUE, MIN_PWM_VALUE, MAX_PWM_VALUE);
-      Pwm_L_position = map(Encoder_value, 0, MAX_ENCODER_VALUE, MIN_PWM_VALUE, MAX_PWM_VALUE);
+         case CALIBRATING:
+            lcd.home ();                   // go home
+            lcd.print("iRis Setting     ");  
+            main_status = SETTING;
+            Encoder_value = 0;
+            break;
       
-      lcd.setCursor ( 0, 1 );        // go to the next line
-      lcd.print ("Pwm :           ");      
-      lcd.setCursor ( 6, 1 );
-      lcd.print(Pwm_R_position);
+         default:
+         case SETTING:
+            lcd.home ();                   // go home
+            lcd.print("iRis Run        ");  
+            main_status = NORMAL;
+            break;
+      }
+   }    
+      
+   switch(main_status)
+   {
+      case NORMAL:
+         /* Get a new sensor event */ 
+         sensors_event_t event_R;
+         sensors_event_t event_L;
    
-      rightEye.write(Pwm_R_position);
-      leftEye.write(Pwm_L_position);
-      delay(10);
+         tsl_R.getEvent(&event_R);
+         tsl_L.getEvent(&event_L);
+
+         lcd.setCursor ( 0, 1 );         /* Print PWM values */
+         lcd.print ("L:       R:    ");  /* Left at 3, right at 11 */
+ 
+         /* Display the results (light is measured in lux) */
+         if (event_R.light)
+         {
+            Pwm_R_position = map(event_R.light, 0, MAX_LIGHT_VALUE, MIN_PWM_VALUE, MAX_PWM_VALUE);
+            
+            lcd.setCursor ( 3, 1 );
+            lcd.print(Pwm_R_position);
+            
+            rightEye.write(Pwm_R_position);
+         }
+   
+         if (event_L.light)
+         {
+            Pwm_L_position = map(event_L.light, 0, MAX_LIGHT_VALUE, MIN_PWM_VALUE, MAX_PWM_VALUE);
+
+            lcd.setCursor ( 11, 1 );
+            lcd.print(Pwm_L_position);
+
+            leftEye.write(Pwm_L_position);
+         }
+         delay(10);
+         break;
+         
+      case CALIBRATING:   
+         if(Encoder_value != Old_encoder_value)
+         {
+            Old_encoder_value = Encoder_value;
+            Pwm_R_position = map(Encoder_value, 0, MAX_ENCODER_VALUE, MIN_PWM_VALUE, MAX_PWM_VALUE);
+            Pwm_L_position = map(Encoder_value, 0, MAX_ENCODER_VALUE, MIN_PWM_VALUE, MAX_PWM_VALUE);
+      
+            lcd.setCursor ( 0, 1 );         /* Print PWM values */
+            lcd.print ("L:       R:    ");  /* Left at 3, right at 11 */
+            lcd.setCursor ( 3, 1 );
+            lcd.print(Pwm_R_position);
+            lcd.setCursor ( 11, 1 );
+            lcd.print(Pwm_L_position);
+   
+            rightEye.write(Pwm_R_position);
+            leftEye.write(Pwm_L_position);
+            delay(10);
+         }
+         break;
+         
+      case SETTING:   
+         if(Encoder_value != Old_encoder_value)
+         {
+            Old_encoder_value = Encoder_value;
+            
+            lcd.setCursor ( 0, 1 );         /* Print Menu */
+            lcd.print ("Menu TBD       ");  
+
+         }
+         break;
+         
    }
 }
 
